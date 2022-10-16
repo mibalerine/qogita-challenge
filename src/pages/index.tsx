b17046ad1e8a89a4c158b675cc74100a87783709
mibalerine <mibalerine@gmail.com>
2022-10-16 21:16:56 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Layout from '../components/Layout';
+import React,{useEffect} from 'react';
+
+// const listItems = products.map((product) =>
+//   <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">{product.name}</li>
+// );
+
+// useEffect(()=>{
+
+// });
+
+const HomePage = () => (
+  <Layout>
+    <h1>Products</h1>
+    {/* <ol>{listItems}</ol> */}
+  </Layout>
+);
+
+export default HomePage;
