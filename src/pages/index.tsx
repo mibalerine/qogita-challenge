f64966d25cfcbfa1f6c6ec7708bf9bd311381b96
mibalerine <mibalerine@gmail.com>
2022-10-16 22:48:41 +0200

    homepage returns products (raw)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
+import React,{useEffect, useState} from 'react';
 import Layout from '../components/Layout';
-import React,{useEffect} from 'react';
+import { ProductsResponse } from '../types';
+import { getProducts } from '../services/products';
 
-// const listItems = products.map((product) =>
-//   <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">{product.name}</li>
-// );
 
-// useEffect(()=>{
 
-// });
+const HomePage = () => {
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
 
-const HomePage = () => (
-  <Layout>
+  const listItems = productsResponse?.results.map((product) =>
+    <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">{product.name}</li>
+  );
+  
+  useEffect(()=>{
+    getProducts()
+      .then((res: ProductsResponse) => setProductsResponse(res))
+  });
+  
+
+  return (<Layout>
     <h1>Products</h1>
-    {/* <ol>{listItems}</ol> */}
-  </Layout>
-);
+    <ol>{listItems}</ol>
+  </Layout>);
+};
 
 export default HomePage;
