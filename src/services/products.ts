1d7637150180a8bb62645e45a73e0489dfbeb24f
mibalerine <mibalerine@gmail.com>
2022-10-17 23:36:02 +0200

    added pagenum to getProducts

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export async function getProducts(){
-    return await axios.get('http://localhost:3000/api/products').then(content => content.data);
+export async function getProducts(pageNum: number = 1){
+    return await axios.get(`http://localhost:3000/api/products?page=${pageNum}`).then(content => content.data);
 }
