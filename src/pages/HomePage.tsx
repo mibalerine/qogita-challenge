c4cce9d9492b33afa9c2ff90cd5ac4a07115d12b
mibalerine <mibalerine@gmail.com>
2022-10-18 15:25:14 +0200

    renaming homepage component filename

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,31 @@
 import React,{useEffect, useState} from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/Layout/Layout';
 import Product from '../components/Product/Product';
 import { ProductsResponse } from '../types';
 import { getProducts } from '../services/products';
-
-
+import Pagination from '../components/Pagination/Pagination';
 
 const HomePage = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  let pageNum: number = 1;
 
-  const listItems = productsResponse?.results.map((product) =>
-    <Product item={product}></Product>
-  );
   
-  useEffect(()=>{
-    getProducts()
+  useEffect(()=>{    
+    getProducts(pageNum)
       .then((res: ProductsResponse) => setProductsResponse(res))
-  });
+  }, []);
+  
+  const listItems = productsResponse?.results.map((product) =>
+    <Product item={product} key={product.gtin}></Product>
+  );
+
   
 
   return (<Layout>
     <h1>Products</h1>
     <div>{listItems}</div>
-    <h2>Total: {productsResponse?.count}</h2>
+
+    <Pagination pageNum={pageNum}></Pagination>
   </Layout>);
 };
 
