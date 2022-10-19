f2ba62564174ce4047f5532f10d243b306e97b39
mibalerine <mibalerine@gmail.com>
2022-10-19 23:41:15 +0200

    styling in Product comp

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -13,9 +13,9 @@ const Product = ({ item }: Props) => {
     <div className="rounded overflow-hidden shadow-lg">
         <img className="w-full" src={item.imageUrl} alt={item.name}/>
         <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{item.name}</div>
+            <div className="font-bold text-l mb-2">{item.name}</div>
             <div className="flex justify-between items-center">
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">{item.recommendedRetailPrice} {item.recommendedRetailPriceCurrency}</span>
+                <span className="text-2xl font-bold text-gray-900 dark:text-white">{item.recommendedRetailPrice} {item.recommendedRetailPriceCurrency}</span>
                 <button onClick={()=> actions.addToCart(item)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
             </div>
         </div>
