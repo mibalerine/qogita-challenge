931709a052c16fa33d7bb0ad41f9ec1a35905cec
mibalerine <mibalerine@gmail.com>
2022-10-19 22:29:41 +0200

    cleanup

--- src/services/products.ts
+++ src/services/products.ts
@@ -3,7 +3,3 @@ import axios from 'axios';
 export async function getProducts(pageNum: number = 1){
     return await axios.get(`http://localhost:3000/api/products?page=${pageNum}`).then(content => content.data);
 }
-
-export async function getProduct(gtin: string){
-    return await axios.get(`http://localhost:3000/api/products/${gtin}`).then(content => content.data);
-}
