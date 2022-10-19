0a3bf9e235867ce7da1196ff97c17fafb6253679
mibalerine <mibalerine@gmail.com>
2022-10-19 22:30:14 +0200

    adding quantity and refactoring

--- src/pages/cart/observer.tsx
+++ src/pages/cart/observer.tsx
@@ -2,18 +2,50 @@ import { useEffect, useMemo, useState } from "react";
 import { Observable } from "../../observers/observable";
 import { Product } from "../../types";
 
-export const cartStore = new Observable<Product[]>([]);
+export interface CartProduct extends Product {
+    quantity?: number;
+}
+
+export const cartStore = new Observable<CartProduct[]>([]);
 
 export const useCart = () => {
-  const [cart, setCart] = useState<Product[]>(cartStore.get());
+  const [cart, setCart] = useState<CartProduct[]>(cartStore.get());
 
     useEffect(() => {
         return cartStore.subscribe(setCart);
     }, []);
 
+    const addToCart = (item: CartProduct) => {
+        const productsIds = cart.map(prod => prod.gtin);
+
+        const idxExistingItem = productsIds.indexOf(item.gtin);
+
+        if(idxExistingItem !== -1){
+            //product already exists in cart. increment quantity.
+
+            let existingProduct = cart[idxExistingItem];
+            if(existingProduct.quantity == undefined){
+                existingProduct.quantity = 0;
+            }
+            
+            existingProduct.quantity++;
+            cartStore.set(cart);
+        }else{
+            //new product to cart. add to cart with quantity = 1.
+            item.quantity = 1;
+            cartStore.set([...cart, item])
+        }
+    }
+
+    const removeFromCart = (item: Product) => {
+        const newCart = cart.filter(prod => prod.gtin !== item.gtin);
+        cartStore.set(newCart);
+    }
+
   const actions = useMemo(() => {
     return {
-      addToCart: (item: Product) => cartStore.set([...cart, item]), 
+      addToCart: addToCart, 
+      removeFromCart: removeFromCart
     }
   }, [cart])
 
