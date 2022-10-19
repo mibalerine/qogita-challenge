67607d74b1cc509e1699ccdfd930427aad4df05e
mibalerine <mibalerine@gmail.com>
2022-10-20 00:59:53 +0200

    implementation of setQuantity action

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,18 @@
+import CartProduct from '../../components/CartProduct/CartProduct';
 import Layout from '../../components/Layout/Layout';
+import { CartProduct as CartProductType, cartStore } from './observer';
+ 
+const CartPage = () => {
+  const cartProducts: CartProductType[] = cartStore.get();
 
-const CartPage = () => (
-  <Layout>
-    <h1>Your Cart</h1>
+  const listItems = cartProducts.map((product: CartProductType) => 
+    <CartProduct item={product} key={product.gtin}/>
+  );
+  
+  return <Layout>
+    <h1 className="font-medium leading-tight text-4xl mt-0 mb-2">Cart</h1>
+    <div>{listItems}</div>
   </Layout>
-);
+};
 
 export default CartPage;
