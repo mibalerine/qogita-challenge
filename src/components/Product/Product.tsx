c2819d7ce4315dde2add1d6389cf4237963d7a3b
mibalerine <mibalerine@gmail.com>
2022-10-19 22:29:25 +0200

    adding addToCart action to product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,11 +1,14 @@
+import { useCart } from '../../pages/cart/observer';
 import { Product as ProductType } from '../../types';
 
 type Props = {
     item: ProductType;
 };
 
-const Product = ({ item }: Props) => (
-    <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+const Product = ({ item }: Props) => {
+    const { actions } = useCart();
+
+    return (<div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img className="p-8 rounded-t-lg" src={item.imageUrl} alt="product image"/>
         </a>
@@ -16,11 +19,11 @@ const Product = ({ item }: Props) => (
             
             <div className="flex justify-between items-center">
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">{item.recommendedRetailPrice} {item.recommendedRetailPriceCurrency}</span>
-                <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                <button onClick={()=> actions.addToCart(item)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
             </div>
         </div>
-    </div>
-  );
+    </div>);
+};
   
   export default Product;
   
