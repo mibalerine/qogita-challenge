920366c4c6d2cd656f6182135891c1e42e53018d
mibalerine <mibalerine@gmail.com>
2022-10-20 21:35:34 +0200

    adding count to Cart in nav bar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,36 +1,42 @@
 import Link from 'next/link';
+import { CartProduct, useCart } from '../../pages/cart/observer';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => (
-<div className="container mx-auto px-4">
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="/" className="flex items-center">
-            <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/r4rybfzzjdlsv93l5j9q" className="mr-3 h-6 sm:h-9" alt="Qogita Logo"/>
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Qogita</span>
-        </a>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-            <Link href="/">
-              <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Products</a>
-            </Link>
+const Layout = ({ children }: Props) => {
+  const { state: cartProducts } = useCart();
 
-            </li>
-            <li>
-            <Link href="/cart">
-              <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cart</a>
-            </Link>
-            </li>
-          </ul>
+  return (<div className="container mx-auto px-4">
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="/" className="flex items-center">
+              <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/r4rybfzzjdlsv93l5j9q" className="mr-3 h-6 sm:h-9" alt="Qogita Logo"/>
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Qogita</span>
+          </a>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+              <Link href="/">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Products</a>
+              </Link>
+
+              </li>
+              <li>
+              <Link href="/cart">
+                <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <span className='mr-2'>Cart</span>
+                  {cartProducts.length ? <span>({cartProducts.length})</span> : null}
+                </a>
+              </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-    {children}
-  </div>
-);
+      </nav>
+      {children}
+    </div>);
+};
 
 export default Layout;
