89900695a123529c317e2708ac4071198cca35be
mibalerine <mibalerine@gmail.com>
2022-10-20 15:44:13 +0200

    basic sum of cart items' prices

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -9,9 +9,12 @@ const CartPage = () => {
     <CartProduct item={product} key={product.gtin}/>
   );
   
+  const sumCartTotal: number = cartProducts.reduce((sum, current) => sum + ((current.quantity ?? 1) * current.recommendedRetailPrice), 0);
+
   return <Layout>
     <h1 className="font-medium leading-tight text-4xl mt-0 mb-2">Cart</h1>
     <div>{listItems}</div>
+    <h2>Total: {sumCartTotal} {cartProducts[0].recommendedRetailPriceCurrency}</h2>
   </Layout>
 };
 
