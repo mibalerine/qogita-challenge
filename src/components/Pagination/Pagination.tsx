31d00152781af8aa266218794d3680a7b305a9b2
mibalerine <mibalerine@gmail.com>
2022-10-20 18:11:14 +0200

    applied pag observer actions and considered page limits

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,29 +1,16 @@
-type Props = {
-    pageNum: number
-};
+import { usePagination } from "../../pages/homepage/paginationObserver";
 
-//TODO: 
-//buttonHandler: pageNum alter must trigger getProducts
-//buttonHandler: consider page limits - dont alter pageNum for first and last page scenario
-//styling
+const prevButtonClass: string = "text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800",
+    nextButtonClass: string = "text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-4 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800";
 
-const Pagination = ({ pageNum }: Props) => {
-    const prevButtonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        pageNum--;
-    };
-
-    const nextButtonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        pageNum++;
-    };
+const Pagination = () => {
+    const { state: {pageNum, totalPages = 1}, actions } = usePagination();
 
     return (<div>
-        <button onClick={prevButtonHandler} type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Previous</button>
-        You are on page {pageNum}
-        <button onClick={nextButtonHandler} type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Next</button>
+        {pageNum > 1 ? <button onClick={actions.decrementPageNum} type="button" className={prevButtonClass}>Previous</button> : null}
+        Page { pageNum }
+        {pageNum < totalPages ? <button onClick={actions.incrementPageNum} type="button" className={nextButtonClass}>Next</button> : null}
       </div>);
-
 };
 
 export default Pagination;
