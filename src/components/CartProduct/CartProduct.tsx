82e6d08e446445e7e4153f869929036eb329c283
mibalerine <mibalerine@gmail.com>
2022-10-20 21:35:17 +0200

    refactoring

--- src/components/CartProduct/CartProduct.tsx
+++ src/components/CartProduct/CartProduct.tsx
@@ -5,20 +5,20 @@ type Props = {
 };
 
 const CartProduct = ({ item }: Props) => {
-    const { actions } = useCart()
+    const { actions } = useCart();
 
     const incrementQuantity = () => {
         if(item.quantity === undefined) item.quantity = 0;
         
         item.quantity++;
-        actions.setProductQuantity(item);
+        actions.setProduct(item);
     };
 
     const decrementQuantity = () => {
         if(item.quantity === undefined || item.quantity === 0)return;
         
         item.quantity--;
-        actions.setProductQuantity(item);
+        actions.setProduct(item);
     };
 
     return (
@@ -31,9 +31,9 @@ const CartProduct = ({ item }: Props) => {
                     <div className="flex-1 min-w-0">
                         <p className="text-sm font-medium text-gray-900 dark:text-white">{item.name}</p>
                         <p className="text-sm text-gray-500 dark:text-gray-400">
-                            <button onClick={incrementQuantity} type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">+</button>
+                            <button onClick={decrementQuantity} type="button" className="py-1 px-2.5 mr-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">-</button>
                             <span>Quantity: {item.quantity}</span>
-                            <button onClick={decrementQuantity} type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">-</button>
+                            <button onClick={incrementQuantity} type="button" className="py-1 px-2.5 ml-3 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">+</button>
                         </p>
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">{item.recommendedRetailPrice} {item.recommendedRetailPriceCurrency}</div>
